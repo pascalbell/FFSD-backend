aa51eab5e0fec26cfed4e4459a91fda1db8c549c
Pascal Bell <pascal.bell@duke.edu>
2023-05-25 16:08:14 -0400

    login started

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,26 @@ import express, {NextFunction, Request, Response} from "express";
 const app = express();
 const PORT: number = 80;
 
-app.listen(PORT, () => console.log(`Running on port ${PORT}`)); //listens for requests on port 80
 
 const arr:string[] = [];
 app.use(express.json());
 app.use(express.urlencoded());
 
-function middleTest(req: Request, res: Response, next: NextFunction) {
-    console.log(req.query);
-    next();
-}
 
-app.get("/login", (req: Request, res: Response) => {
+app.get("/api/login", (req: Request, res: Response) => {
     res.send(arr);
 })
 
 
-app.post("/login", (req: Request, res: Response) => {
+app.post("/api/login", (req: Request, res: Response) => {
+    if (!req.body.username || !req.body.password) {
+        res.status(422).json({error: true});
+        return;
+    }
     console.log(req.body);
     arr.push(req.body);
-    res.send(201);
+    res.send(201);  //status didnt work for some reason
 });
+
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`)); //listens for requests on port 80
