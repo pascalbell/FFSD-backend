c450a9f197e32a8479286a7c4a9701dbd2f241a3
Pascal Bell <pascal.bell@duke.edu>
2023-05-29 11:36:59 -0400

    working

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import express, {NextFunction, Request, Response} from "express";
 import session, { SessionData } from 'express-session';
 const PORT: number = 80;
 const app = express();
-const users:string[] = [];                  //change this to the database later
+const users:User[] = [];                  //change this to the database later
+
+interface User extends SessionData {
+    username?: string;
+    password?: string;
+}
 
 app.use(express.json());                    //allows parsing on json
 app.use(express.urlencoded());
@@ -18,15 +23,21 @@ app.get("/api/login", (req: Request, res: Response) => {
     res.send(users);
 })
 
-
 app.post("/api/login", (req: Request, res: Response) => {
     const { username, password } = req.body;
     const user = { username, password };
+    const thisSession = req.session;
 
     if (!user.username || !user.password) {
         res.status(422).json({error: true});
         return;
     }
+
+    if (thisSession) {
+        (req.session as User).username = user.username;
+        (req.session as User).password = user.password;
+    }
+
     console.log(req.body);
     users.push(req.body);
     res.status(201).send();  //status didnt work for some reason
