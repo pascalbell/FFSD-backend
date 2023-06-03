5580b2aafb374d541b5a70f61a9e0258a61a58d3
Pascal Bell <pascal.bell@duke.edu>
2023-06-03 16:38:56 -0400

    fixed /api/me after deleting database doc

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,7 @@ import {Request, Response, Router} from "express";
 import { SessionData } from 'express-session';
 import UserModel from "./models/User";
 import bcrypt from 'bcryptjs';
-import { ErrMsg } from "./util";
+import { ErrMsg, cleanUser } from "./util";
 import { ObjectId } from "mongoose";
 const router = Router();
 const salt = bcrypt.genSaltSync();
@@ -11,6 +11,21 @@ export interface User extends SessionData {        //Session data
     _id?: String;
 }
 
+type JSONKey = string | boolean | number | Array<JSONKey> | JSONObject;
+
+type JSONObject = {
+    [key: string]: JSONKey
+};
+
+const destroySession = (req: Request): Promise<void>=>{
+    return new Promise((resolve, reject)=>{
+        req.session.destroy((err) => {
+            if (err) return reject(ErrMsg(err));
+            resolve();
+        })
+    });
+};
+
 router.get("/me", async (req, res) => {
     const user = req.session as User;
     if (!user._id) {
@@ -18,11 +33,14 @@ router.get("/me", async (req, res) => {
         return;
     }
     const userDB = await UserModel.findOne({ _id: user._id }) as any;
-    const copyUser = Object.assign({}, userDB);
-    delete copyUser._doc.password;
-    delete copyUser._doc.__v;
-    res.status(200).json(copyUser._doc);       //change this last field to access diff fields
 
+    if(!userDB){
+        destroySession(req);
+        res.status(404).json(ErrMsg("Not logged in!"));
+        return;
+    }
+    
+    res.status(200).json(cleanUser(userDB._doc));       //change this last field to access diff fields
 })
 
 router.post("/login", async (req, res) => {
@@ -33,16 +51,16 @@ router.post("/login", async (req, res) => {
     
     const user = req.session as User;
     const username = req.body.username;
-    const userDB = await UserModel.findOne({ username });
+    const userDB: any = await UserModel.findOne({ username });
     if (userDB && bcrypt.compareSync(req.body.password, userDB.password)) {
         // Passwords match
         // Perform the necessary actions when the login is successful
         if(user._id) {
-            res.status(201).json(ErrMsg('already logged in!'));
+            res.status(201).json(ErrMsg('Already logged in!'));
             return;
         }
         user._id = userDB._id.toString();        //every entry in DB has _id, only logged in sessions have a sessionID
-        res.status(200).json({ message: 'Login successful' });
+        res.status(200).json(cleanUser(userDB._doc));
         return;
     }    
     res.status(422).json(ErrMsg('Wrong password or username!'));
@@ -53,7 +71,7 @@ router.post("/signup", async (req: Request, res: Response) => {
     
     if (!username || !password || !email || typeof username != "string" || typeof password != "string" || typeof email != "string") {
         //add other fields above if need more
-        res.status(422).json(ErrMsg("signup must have username, password, and email"));
+        res.status(422).json(ErrMsg("Signup must have a valid username, password, and email."));
         return;
     }
     const userDB = await UserModel.findOne({ $or: [{ username }, { email }]});
@@ -66,15 +84,18 @@ router.post("/signup", async (req: Request, res: Response) => {
     const hashedPass = bcrypt.hashSync(password, salt);
     await UserModel.create({ username, password: hashedPass, email });
 
-    res.status(201).json({ message: "User created!" });
+    res.status(201);
 });
 
 router.post("/signout", async (req: Request, res: Response) => {
-    req.session.destroy((err) => {
-        if (err) return res.status(500).json(ErrMsg("error signing out"));
-        
-        res.status(200).json({ message: "signed out sucessfully" });
-    })
+    destroySession(req)
+        .then(() => {
+            res.status(200);
+        })
+        .catch((e: JSONObject) => {
+            console.log(e);
+            res.status(500).json("Something went wrong signing out");
+        })
 })
 
 export default router;
--- src/util.ts
+++ src/util.ts
@@ -3,3 +3,9 @@ export const ErrMsg = (msg: any) => {
     return {error: msg};
 }
 
+export const cleanUser = (userDB: any)=>{
+    const newUserDB = Object.assign({}, userDB);
+    delete newUserDB.password;
+    delete newUserDB.__v;
+    return newUserDB;
+}
