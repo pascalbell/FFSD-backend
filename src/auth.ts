30ff6dcc8675427193457d5a5435c636fcc564a4
Pascal Bell <pascal.bell@duke.edu>
2023-06-06 12:14:38 -0400

    email sending need to add verification

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,7 @@ import {Request, Response, Router} from "express";
 import { SessionData } from 'express-session';
 import UserModel from "./models/User";
 import bcrypt from 'bcryptjs';
-import { ErrMsg, cleanUser, decrypt, encrypt } from "./util";
+import { ErrMsg, cleanUser, decrypt, encrypt, sendVerification } from "./util";
 import crypto from 'crypto';
 
 const router = Router();
@@ -86,11 +86,13 @@ router.post("/signup", async (req: Request, res: Response) => {
 
     const encryptedEmail = encrypt(email);
     const hashedPass = bcrypt.hashSync(password, salt);
-    await UserModel.create({ username,
-                             password: hashedPass,
-                             hashed_email: hashedEmail,
-                             encrypted_email: encryptedEmail,
-                             email_token: crypto.randomBytes(64).toString('hex') });
+    const user = await UserModel.create({ 
+                                        username,
+                                        password: hashedPass,
+                                        hashed_email: hashedEmail,
+                                        encrypted_email: encryptedEmail,
+                                        email_token: crypto.randomBytes(64).toString('hex') });
+    sendVerification(user);
 
     res.status(201).json({});
 });
@@ -109,16 +111,16 @@ router.post("/signout", async (req: Request, res: Response) => {
 router.post("/verify-email", async (req: Request, res: Response) => {
     try {
         const emailToken = req.body.email_token;
+        console.log(emailToken);
         if (!emailToken) return res.status(404).json(ErrMsg("No email token provided"));
-        
-        const user: any = await UserModel.findOne({ email_token: emailToken });
+
+        const user: any = await UserModel.findOneAndUpdate(
+            { email_token: emailToken },
+            { $set: { email_token: null, email_valid: true} },
+            { new: true }
+        );
+
         if (!user) return res.status(404).json(ErrMsg("Not valid token"));
-
-        delete user._doc.email_token;
-        user._doc.email_valid = true;
-        console.log(decrypt(user._doc.encrypted_email));
-        await user.save();
-
         res.status(201).json(cleanUser(user._doc));
     } catch (error) { console.error(error); }
 });
--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,7 @@
 import crypto, { BinaryLike } from 'crypto';
+import nodemailer from "nodemailer";
 
 const encryptionKey = Buffer.from(process.env.ENCRYPTION_KEY!, 'hex');
-
 
 export const ErrMsg = (msg: any) => {
     return {error: msg};
@@ -30,4 +30,31 @@ export const decrypt = (inp: {string: string, iv: any}) => {
     const decipher = crypto.createDecipheriv('aes-256-cbc', encryptionKey, inp.iv.buffer);
     const decryptedEmail = decipher.update(inp.string, 'hex', 'utf8');
     return decryptedEmail + decipher.final('utf8');
+}
+
+export const mailTransporter = () => {
+    const transporter = nodemailer.createTransport({
+        service: "outlook",
+        auth: {
+            user: "testing-ffsd@outlook.com",
+            pass: "testingFFSD"
+        }
+    });
+    return transporter;
+}
+
+export const sendVerification = (user: any) => {
+    const transporter = mailTransporter();
+
+    const mailOptions = {
+        from: '"FFSD Test Verification" <testing-ffsd@outlook.com>',
+        to: "pascalbell16@gmail.com",
+        subject: "Verify your email...",
+        html: `<p>Hello ${user.name}</p>`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) return console.log(error);
+        console.log("verification sent");
+    });
 }
