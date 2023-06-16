ab522117460cd749a5e5d1802387df8baa3e1371
Pascal Bell <pascal.bell@duke.edu>
2023-06-16 18:36:17 -0400

    removed console.log statement

--- src/auth.ts
+++ src/auth.ts
@@ -64,7 +64,6 @@ router.post("/login", async (req, res) => {
         }
         
         user._id = userDB._id.toString();        //every entry in DB has _id, only logged in sessions have a sessionID
-        console.log(cleanUser(userDB._doc));
         res.status(200).json(cleanUser(userDB._doc));
         return;
     }    
